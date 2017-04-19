59b6e354191f15c262a51ad1995f9672578b64b7
Michael Handschuh <mhandschuh@gmail.com>
2017-04-19 19:04:38 -0400

    Add BrokerageSpecifier
    This type is intended to be used to provide the algorithm with
    the data required for routing orders and history requests to
    brokerages.

---
+++ Common/Brokerages/BrokerageSpecifier.cs
@@ -0,0 +1,29 @@
+﻿namespace QuantConnect.Brokerages
+{
+    /// <summary>
+    /// Provides access to details of a specific brokerage
+    /// </summary>
+    public class BrokerageSpecifier
+    {
+        /// <summary>
+        /// Gets the brokerage account name
+        /// </summary>
+        public string Account { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="BrokerageName"/>
+        /// </summary>
+        public BrokerageName Name { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BrokerageSpecifier"/> class
+        /// </summary>
+        /// <param name="name">The brokerage's name</param>
+        /// <param name="account">The user's account id</param>
+        public BrokerageSpecifier(BrokerageName name, string account)
+        {
+            Name = name;
+            Account = account;
+        }
+    }
+}
